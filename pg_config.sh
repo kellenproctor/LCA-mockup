308b18b316ece12faf2c71428f568b244e8d7e1c
Kellen Proctor <kellenproctor@gmail.com>
2016-03-21 10:46:07 +0800

    Added vagrant config files

---
+++ pg_config.sh
@@ -0,0 +1,12 @@
+apt-get -qqy update
+apt-get -qqy upgrade
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
+
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+echo -e $vagrantTip > /etc/motd

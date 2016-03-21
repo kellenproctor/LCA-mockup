36cb83726291ef5e108fedb9561f2deb56a80a7d
Kellen Proctor <kellenproctor@gmail.com>
2016-03-21 14:38:37 +0800

    Basic routing, moved header.html template inclusion to main.html

--- mockup/__init__.py
+++ mockup/__init__.py
@@ -2,8 +2,24 @@ from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/team")
+def team():
+    return render_template('team.html')
+
+@app.route("/news")
+def news():
+    return render_template('news.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.secret_key = 'super_secret_key'

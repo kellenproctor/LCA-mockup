5da42fc198669046e792fecc330763e2236d43d6
Kellen Proctor <kellenproctor@gmail.com>
2016-03-21 13:33:58 +0800

    Added static and templates directories, basic config of application.py

--- mockup/application.py
+++ mockup/application.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.secret_key = 'super_secret_key'
